17f84e5f64b9d6701d923723833eab52a72fc763
배주호 <ss873higxz@gmail.com>
2026-04-03 12:00:07 +0900

    260403 5주차 수업

--- 서로 다른 데이터 타입 처리 객체_배주호.R
+++ 서로 다른 데이터 타입 처리 객체_배주호.R
@@ -1,3 +1,19 @@
+setwd("C:/r_data")
+Sys.setlocale('LC_ALL', 'Korean')
+
+library(lubridate)
+
+#서로다른 데이터 타입 처리 객체
+# 1. list
+# - 백터와 비슷한 형태(키, 값)으로 저장
+# -$의 의미 : 속해 있다.
+
+l1 = list(name='홍길동',
+          addr='서울',
+          tel='010-123-456',
+          pay=10000)
+l1
+
 # 특정 키만 조회
 l1$addr
 
@@ -104,3 +120,117 @@ df1
 
 class(df1)
 str(df1)
+
+# 데이터 수정 : 변수명 바꾸기
+install.packages("dplyr")
+library(dplyr)
+
+df1 = data.frame(var1=c(1,2,1),
+                 var2=c(2,3,3))
+df2=df1
+
+df2 =rename(df2, v2=var2)
+df2
+
+# 변수 조합해서 파생변수 만들기
+df1
+
+df1$var_sum = df1$var1 + df1$var2
+df1
+
+install.packages("ggplot2")
+library(ggplot2)
+
+mpg
+View(mpg)
+
+class(mpg)
+str(mpg)
+head(mpg)
+tail(mpg)
+
+mpg1=mpg
+mpg1 = rename(mpg1, city=cty)
+mpg1 = rename(mpg1, highway=hwy)
+View(mpg1)
+
+
+setwd("C:/r_data")
+
+# scan() : 텍스트 파일 읽어서 배열에 저장
+s1 = scan('scan_1.txt')
+s1
+s1[2]
+
+
+s2 = scan('scan_2.txt')
+s2
+
+s2 = scan('scan_2.txt', what='')
+s2
+
+s3 = scan('scan_3.txt')
+s3
+
+s3 = scan('scan_3.txt', what='')
+s3
+
+s4 = scan('scan_4.txt', what='')
+s4
+
+input = scan()
+input
+input[3]
+
+input2 = readline("R U OK? ")
+input2
+
+# readLines() : 파일을 읽어서 벡터에 저장
+input3 = readLines('scan_4.txt')
+input3
+
+# read.table() : 데이터를 읽어서 데이터프레임에 저장
+# - 주석이나 공백을 제외하고 읽는다
+# 기본적으로 컬럼명이 없다고 한다
+
+f = read.table('fruits.txt')
+f
+
+f = read.table('fruits.txt', header = T)
+f
+
+f2 = read.table('fruits_2.txt', skip=2)
+f2
+
+f2 = read.table('fruits_2.txt', nrows=2)
+f2
+
+f2 = read.table('fruits_2.txt', skip=2, nrows=2)
+f2
+
+# read.csv() : csv파일 읽기
+# read.table()와 다르게 기본적으로 컬럼명이 있다고 판단
+
+f3 = read.csv('fruits_3.csv')
+f3
+
+f3 = read.csv('fruits_4.csv')
+f3
+
+f3 = read.csv('fruits_4.csv', header = F)
+f3
+
+lab = c('NO', 'NAME', 'PRICE', 'QTY')
+f3 = read.csv('fruits_4.csv', header = F, col.name = lab)
+f3
+
+
+# read.csv() -> write.csv(), read.table() -> write.table(), readLines() -> write()
+t = read.csv('csv_test.txt')
+t
+write.csv(t, 'csv_t.csv')
+t1 = read.csv('csv_t.csv')
+t1
+
+t2 = read.table("csv_test.txt", sep=',', header = T)
+t2
