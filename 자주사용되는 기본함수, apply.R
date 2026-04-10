50b2003199b14f6076de052c6e975a5e98904fbe
배주호 <ss873higxz@gmail.com>
2026-04-10 11:56:33 +0900

    260410 6주차 수업_배주호

--- 자주사용되는 기본함수, apply.R
+++ 자주사용되는 기본함수, apply.R
@@ -2,8 +2,10 @@ setwd("C:/r_data")
 Sys.setlocale('LC_ALL', 'Korean')
 
 library(lubridate)
-
-
+library(dplyr)
+library(plyr)
+library(ggplot2)
+library(googleVis)
 
 # 자주사용되는 기본 함수
 # aggregate() : 다양한 함수를 사용하여 셜과 출력
@@ -108,3 +110,185 @@ v3 = c(100,200,300,400,500)
 
 mapply(sum, v1,v2,v3)
 mapply(max, v1,v2,v3)
+
+# 사용자 정의 함수 : 함수를 직접 만들어서 사용
+# 함수명 = function(인수 또는 입력값) {
+#     수식 1
+#     수식 2
+#     ...
+#     return(반환값)
+# }
+#
+# 1. 입력값이 없는 경우
+
+
+my1 = function(){
+  return(10)
+}
+
+my1
+my1()
+
+# 2. 입력값(인자)이 있는 경우
+
+my2 = function(a){
+  b = a^2
+  return(b)
+}
+
+my2(3)
+
+
+my3 = function(a, b){
+  c = abs(a-b)
+  return(c)
+}
+
+my3(3, 4)
+
+
+my4 = function(a, b){
+  if (a > b) {
+    c = a-b
+  } else {
+    c = b-a
+  }
+  return(c)
+}
+
+my4(3, 4)
+
+# sort() : 정렬
+s1 = Fruits$Sales
+s1
+
+sort(s1)
+sort(s1, decreasing = T)
+
+# plyr() : 원본 데이터를 분석하기 쉬운 형태로 나누어서 다시 새로운 형태로 만들어주는 패키지
+# - apply()함수를 확장
+# - ply(data, 기준컬럼, 함수)란, 함수앞에 두글자 : 첫글자는 입력될 데이터 유형,
+# 두번째 글자는 출력될 데이터 유형
+# - d: dataframe, a : array(matrix), l : list
+# - dlply(), laply()
+
+install.packages('plyr')
+library(plyr)
+
+f = read.csv('fruits_10.csv')
+f
+
+# summarise : 기존 컬럼의 데이터끼리 모은 후 함수를 적용(sql의 group by와 유사)
+# 년도별로 최고판매량과 최저가격을 구하라.
+ddply(f, 'year', summarise, max_qty = max(qty), min_price=min(price))
+
+
+# 년도별, 과일이름별로 최고 판매량과 최저가격을 구하라.
+ddply(f, c('year', 'name'), summarise, max_qty = max(qty), min_price=min(price))
+
+# transform : 만약 동일한 컬럼이 아닌 각 행별로 연산을 수행해서 해당값을 함께 출력해야 할 경우
+# - 즉, 주어진 데이터프레임에서 기준 컬럼으로 모은 후 계산해서 출력하고 싶은 경우 summarise,
+# 다른 계산을 해서 각 행별로 각각 출력하고 싶은 경우 transform
+f
+
+# pct_qty는 해당 과일의 판매수량이 기준 컬럼으로 합계한 총 판매개수대비 몇 %를 차지하는지 계산
+ddply(f, 'name', transform, sum_qty = sum(qty), pct_qty = round((qty*100)/sum(qty),2))
+
+# dplyr() : plyr()와 동시에 사용한다면 충돌의 위험성
+# 특성함수
+# 1. filter : 조건을 줘서 필터링, 행출력
+# 2. select : 특정 컬럼만 선택
+# 3. arraange : 정렬
+# 4. mutate : 새로운 변수 생성
+# 5. summarise(with group_by) : 주어진 데이터를 집계(min, max, mean, count)
+
+install.packages("dplyr")
+library(dplyr)
+
+d1 = read.csv('야구성적.csv')
+d1
+View(d1)
+
+# filter
+# 경기수가 120경기 이상인 선수
+d2 = filter(d1, 경기>=120)
+d2
+
+# 경기수가 120경기 이상이면서 득점도 80점 이상인 선수
+d3 = filter(d1, 경기>=120 & 득점 >=80)
+d3
+
+# 포지션이 1루수이거나 3루수인 선수
+d4 = filter(d1, 포지션=='1루수' | 포지션=='3루수')
+d4
+
+d4 = filter(d1, 포지션 %in% c('1루수', '3루수'))
+d4
+
+# select
+# 선수명, 포지션, 팀 데이터만 조회
+select(d1, 선수명, 포지션, 팀)
+
+# 순위~타수까지 조회
+select(d1, 순위:타수)
+
+# 특정 컬럼제외
+select(d1, -홈런, -타점, -도루)
+
+# %>% : 여러 문장을 조합해서 사욜할 때
+# 선수명, 팀, 경기, 타수를 조회하되 타수가 400이상인 선수
+d1 %>%
+  filter(타수 >= 400) %>%
+  select(선수명, 팀, 경기, 타수)
+
+#arrange
+d1 %>%
+  filter(타수 >= 400) %>%
+  select(선수명, 팀, 경기, 타수) %>% 
+  arrange(타수)
+
+d1 %>%
+  filter(타수 >= 400) %>%
+  select(선수명, 팀, 경기, 타수) %>% 
+  arrange(desc(타수))
+
+# mutate
+d1 %>%
+  select(선수명, 팀, 경기, 타수) %>% 
+  mutate(경기X타수 = 경기*타수) %>% 
+  arrange(desc(경기X타수))
+
+# 안타율 컬럼생성 : 안타/타수 구해서 할푼리로 출력
+d1 %>%
+  select(선수명, 팀, 경기, 안타, 타수) %>% 
+  mutate(안타율 = round(안타/타수,3)) %>% 
+  arrange(desc(안타율))
+
+# summarise
+# 팀별로 평균 경기 횟수
+d1 %>% 
+  group_by(팀) %>% 
+  summarise(avg = mean(경기, na.rm=T))
+
+d1 %>% 
+  group_by(팀) %>% 
+  dplyr::summarise(avg = mean(경기, na.rm=T))
+
+d1 %>% 
+  group_by(팀) %>% 
+  dplyr::summarise(across(경기, mean))
+
+d1 %>% 
+  group_by(팀) %>% 
+  dplyr::summarise(across(경기, ~mean(., na.rm=T)))
+
+# 여러컬럼을 대상으로 집계
+# 팀별로 경기와 타수의 평균
+
+d1 %>% 
+  group_by(팀) %>% 
+  dplyr::summarise(across(c(경기, 타수), mean))
+
+d1 %>% 
+  group_by(팀) %>% 
+  dplyr::summarise(across(c(경기, 타수), ~mean(., na.rm=T)))
